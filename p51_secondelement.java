b34b1d4e18b1a67a34524f4a409caa864d6a7faf
ManjuMuralidharan <manju1695@gmail.com>
2016-08-29 21:49:36 +0530

    Add files via upload

---
+++ p51_secondelement.java
@@ -0,0 +1,16 @@
+package p51_secondelement;
+
+import java.util.Arrays;
+
+public class p51_secondelement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a={1,4,2,5,7,6};
+		Arrays.sort(a);
+		int smallest=a[1];
+		System.out.println("Smallest element - "+smallest);
+
+	}
+
+}
